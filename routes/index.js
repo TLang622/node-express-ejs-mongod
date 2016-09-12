0be0552adbc8afadc919d8e70e8bba1a9b154924
lang <linxianlang@vispractice.com>
2016-09-12 16:07:23 +0800

    api生成，ajax调用api,ejs绑定json数据

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ var user = require('../models/user').user;
 mongoose.connect('mongodb://localhost/hello');
 
 var userData = {};
+var result = {};
+result.data = {};
+
+//页面路由
 /* GET home page. */
 router.get('/', function(req, res) {
   	res.render('index', { title: 'index' });
@@ -20,21 +24,8 @@ router.get('/logout', function(req, res) {
   	res.render('logout', { title: 'logout' });
 });
 
-/*hompage*/
-router.post('/homepage', function(req, res) {
-	var query_doc = {userid: req.body.userid, password: req.body.password};
-	(function(){
-		user.findOne(query_doc, function(err, doc){
-			if(doc){
-				console.log(query_doc.userid + ": login success in " + new Date());
-				res.render('homepage', { title: 'homepage' });
-				userData = doc;
-			}else{
-				console.log(query_doc.userid + ": login failed in " + new Date());
-				res.redirect('/');
-			}
-		});
-	})(query_doc);
+router.get('/list', function(req, res) {
+  	res.render('list', { title: 'list', users: {} });
 });
 
 /*add*/
@@ -42,16 +33,41 @@ router.get('/add', function(req, res) {
 	res.render('add', { title: 'add' });
 })
 
+/*hompage*/
+router.get('/homepage', function(req, res) {
+	res.render('homepage', { title: 'homepage', userData });
+});
+
+//api，其实路由和api都差不多的，路由是单纯请求页面，api则是对数据库操作，然后返回json
 router.post('/add', function(req, res) {
 	var query_doc = {userid: req.body.userid, password: req.body.password};
 	var newUser = new user(query_doc);
 	newUser.save(function(err) {
 		if(err) {
-			console.log('error')
+			result.status = -1;
+			res.send(result);
 		}else {
-			res.render('index', { title: 'index' });
+			result.status = 1;
+			result.data = query_doc;
+			res.send(result);
 		}
 	})
+})
+
+router.post('/login', function(req, res) {
+	var query_doc = {userid: req.body.userid, password: req.body.password};
+	user.findOne(query_doc, function(err, doc){
+			if(doc){
+				userData = doc;
+				result.data = doc;
+				result.status = 1;
+				res.send(result)
+			}else{
+				//res.redirect('/');
+				result.status = -1;
+				res.send(result);
+			}
+		});
 })
 
 router.post('/modify', function(req, res) {	
@@ -59,35 +75,68 @@ router.post('/modify', function(req, res) {
 		$set: {password: req.body.password}
 	}, function(err) {
 		if(err){
-			console.log(err)
+			result.status = -1;
+			res.send(result);
 		}else{
-			console.log('修改成功');
-			res.render('homepage', { title: 'homepage' });
+			result.status = 1;
+			result.data = {
+				userid: userData._id,
+				password: req.body.password
+			};
+			res.send(result);
 		}
 	})
 })
 
-router.get('/list', function(req, res) {
+router.get('/getUsers', function(req, res) {
 	user.find(function(err, doc) {
 		if(doc) {
-			res.render('list', { users: doc});
+			result.status = 1;
+			result.data = doc;
+			res.send(result);
 		}else {
-			res.render('list', { users: {}});
+			result.status = -1;
+			res.send(result);
 		}
 	})
 })
 
+//第一种del api的写法
 router.get('/del/:id', function(req, res) {
-	user.remove({_id: req.params.id}, function(err, result) {
-		if(result) {
-			user.find(function(err, doc) {
-				if(doc) {
-					res.render('list', { users: doc});
-				}
-			})
+	user.remove({_id: req.params.id}, function(err, doc) {
+		if(doc) {
+			result.status = 1;
+			result.data = doc;
+			res.send(result);
+		}else{
+			result.status = -1;
+			res.send(result);
 		}
 	})
 })
 
+//第二种del api的写法
+router.post('/del', function(req, res) {
+	var query_doc = {userid: req.body.userid};
+	user.findOne(query_doc, function(err, doc){
+			if(doc){
+				user.remove({_id: doc._id}, function(err, doc2) {
+					if(doc2) {
+						result.status = 1;
+						result.data = query_doc;
+						res.send(result);
+					}else{
+						result.status = -1;
+						result.data.text = '删除失败'
+						res.send(result);
+					}
+				})
+			}else{
+				result.status = -1;
+				result.data.text = '没找到用户'
+				res.send(result);
+			}
+		});
+})
 
 module.exports = router;
